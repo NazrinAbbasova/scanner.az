40d2d2722aecf7b113b48c299fad0b3cf775aed3
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-04-30 23:44:08 +0400

    updated register page

--- resources/views/front/auth/register.blade.php
+++ resources/views/front/auth/register.blade.php
@@ -1,9 +1,12 @@
 @extends('front.layout.master')
 @section('content')
+
 <section>
   <div class="container">
     <div class="auth-box">
-        <div class="form-container">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-container">
             <div class="register-form">
               <h2>Qeydiyyat</h2>
               <div class="account-type">
@@ -13,35 +16,38 @@
               <form>
                 <div class="applicant-form">
                   <label for="name">Ad</label>
-                  <input type="text" placeholder="Adınızı daxil edin" />
+                  <input type="text" placeholder="Adınızı daxil edin">
                   <label for="surname">Soyad</label>
-                  <input type="text" placeholder="Soyadınızı daxil edin" />
+                  <input type="text" placeholder="Soyadınızı daxil edin">
                   <label for="email">Email</label>
-                  <input type="email" placeholder="Emailinizi daxil edin" />
+                  <input type="email" placeholder="Emailinizi daxil edin">
                   <label for="password">Şifrə</label>
-                  <input type="password" placeholder="Şifrə təyin edin" />
+                  <input type="password" placeholder="Şifrə təyin edin">
+                  <button type="submit">Qeydiyyatdan keç</button>
                 </div>
-  
+              </form>
+
+              <form>
                 <div class="company-form">
                   <label for="company-name">Şirkət adı</label>
-                  <input type="text" placeholder="Şirkət adını daxil edin" />
+                  <input type="text" placeholder="Şirkət adını daxil edin">
                   <label for="mobile">Mobil nömrə</label>
-                  <input type="text" placeholder="Nömrənizi daxil edin" />
+                  <input type="text" placeholder="Nömrənizi daxil edin">
                   <label for="email">Email</label>
-                  <input type="email" placeholder="Emailinizi daxil edin" />
+                  <input type="email" placeholder="Emailinizi daxil edin">
                   <label for="password">Şifrə</label>
-                  <input type="password" placeholder="Şifrə təyin edin" />
+                  <input type="password" placeholder="Şifrə təyin edin">
+                  <button type="submit">Qeydiyyatdan keç</button>
                 </div>
-                <button type="submit">Qeydiyyatdan keç</button>
               </form>
             </div>
           </div>
-      
-      @include('front.auth.imagebox')
-
+        </div>
+        <div class="col-md-6">
+          @include('front.auth.imagebox')
+        </div>
+      </div>
     </div>
-
-  </div>
   </div>
 </section>
 
