4f434bf27032ef7572848d9a1786dbf199b72a6c
Ismayil Mirzayev <ismayil.mirzayev0@gmail.com>
2021-05-03 14:52:56 +0400

    fixed auth views

--- resources/views/front/auth/login.blade.php
+++ resources/views/front/auth/login.blade.php
@@ -5,11 +5,9 @@
 <section>
 
   <div class="container">
-
     <div class="auth-box">
-      
       <div class="row">
-
+        
         <div class="col-md-6">
           <div class="form-container">
             <div class="login-form">
@@ -20,10 +18,10 @@
                 <input type="email" name="email" placeholder="Emailinizi daxil edin">
                 <label for="password">Şifrə *</label>
                 <input type="password" name="password" placeholder="Şifrənizi daxil edin">
+                <button type="submit">Daxil ol</button>
                 <a href="{{ route('forgot') }}">
                   <div class="forgot-password">Şifrəni unutdum</div>
                 </a>
-                <button type="submit">Daxil ol</button>
               </form>
             </div>
           </div>
@@ -34,9 +32,7 @@
         </div>
 
       </div>
-
     </div>
-
   </div>
 
 </section>
--- resources/views/front/inc/header.blade.php
+++ resources/views/front/inc/header.blade.php
@@ -1,12 +1,14 @@
 <header>
   <div class="container">
     <div class="row">
-      <div class="logo col-md-2">
-        <a href="{{ route('home') }}">
-          <img src="{{ asset('static/front/images/logo.png') }}" alt="" />
-        </a>
+      <div class="col-md-3">
+        <div class="logo">
+          <a href="{{ route('home') }}">
+            <img src="{{ asset('static/front/images/logo.png') }}" alt="" />
+          </a>
+        </div>
       </div>
-      <div class="nav col-md-10">
+      <div class="nav col-md-9">
         <a href="{{ route('about') }}">Haqqımızda</a>
         <a href="{{ route('how-it-works') }}">Sistem necə işləyir?</a>
         <a href="{{ route('contact') }}">Əlaqə</a>
